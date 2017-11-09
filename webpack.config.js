e4f0ccef59fda276c98888b6585d17c02968c4cc
eenewbsauce <ryan.m.tracey@gmail.com>
2017-11-09 14:59:02 -0500

    feat(ejs): add support for this

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,30 @@
 var webpack = require('webpack');
+const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: './lib',
+        path: path.resolve(__dirname, 'dist'),
         filename:  'app.bundle.js'
     },
-    module: {
-        loaders: [
-            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+    plugins: [
+        new HtmlWebpackPlugin(
+            {
+                template: 'ejs-render-loader?name=ryan!./src/index.ejs',
+                options: {
+                    name: 'ryan'
+                }
+            }
+        )
+    ],
+    module: {         
+        rules: [
+            { 
+                test: /\.js$/, 
+                exclude: /node_modules/, 
+                use: "babel-loader" 
+            }               
         ]
     }   
 }
